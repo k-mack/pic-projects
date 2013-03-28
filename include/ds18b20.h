b6fde3cc9aa52fbce636d0149ca8882e58ff13d6
Kevin Macksamie <kevin.macksamie@gmail.com>
2013-03-28 00:24:26 -0400

    Find devices implemented. Conversion of temperature needs ROM address.

--- include/ds18b20.h
+++ include/ds18b20.h
@@ -16,23 +16,25 @@
 #define DS18B20_ALARM_SEARCH        0xEC // Identical to Search ROM command except that only slaves with a set alarm will respond
 
 // DS18B20 Function Command Set
-#define DS18B20_CONVERT_T           0x44 // Initiates temperature conversion
+#define DS18B20_CONVERT_TEMP        0x44 // Initiates temperature conversion
 #define DS18B20_READ_SCRATCHPAD     0xBE // Reads entire scratchpad including the CRC byte
 #define DS18B20_WRITE_SCRATCHPAD    0x4E // Writes data into scratchpad bytes 2, 3, and 4 (T_H, T_L, and configuration registers)
 #define DS18B20_COPY_SCRATCHPAD     0x48 // Copies T_H, T_L, configuration register data from the scratchpad to EEPROM
 #define DS18B20_RECALL_E2           0xB8 // Recalls T_H, T_L, and configuration register data from EEPROM to the scratchpad
 #define DS18B20_READ_POWERSUPPLY    0x84 // Signals DS18B20 power supply mode to the master
 
-#define DS18B20_STATUS_NO_PRESENCE  0x01 // Set when presence signal was not returned by device
+#define MAX_TEMP_SENSORS 1
 
-typedef struct ds18b20
+typedef struct temp_sensors
 {
-    owire_t *device;        // the DS18B20 1-Wire sensor
-    uint8_t scratchpad[9];  // contents read from scratchpad
-    uint8_t status;         // status bits
-} ds18b20_t;
+    owire_t *bus;                       // the 1-Wire bus used for the sensors
+    uint8_t ROMS[MAX_TEMP_SENSORS][8];  // 1-Wire sensors' ROMS
+} temp_sensors_t;
 
-void ds18b20_convert_temp(ds18b20_t *sensor);
+void ds18b20_find_devices(temp_sensors_t *sensors);
+void ds18b20_convert_temp(temp_sensors_t *sensors, uint8_t *ROM);
+uint8_t ds18b20_temp_hi();
+uint8_t ds18b20_temp_lo();
 
 #endif	/* DS18B20_H */
 
--- src/ds18b20.c
+++ src/ds18b20.c
@@ -5,31 +5,169 @@
 
 #include "ds18b20.h"
 
-void ds18b20_convert_temp(ds18b20_t *sensor)
+uint8_t scratchpad[9];      // latest scratchpad read
+uint8_t latest_ROM[8];      // latest collected ROM from search ROM command
+uint8_t done;               // search ROM done flag
+uint8_t last_discrepancy;   // search ROM last found discrepancy
+
+void setup()
 {
-    // TODO: disable interrupts while communicating to device
-    uint8_t lcv;
-    if (owire_reset_pulse(sensor->device))
+    last_discrepancy = 0;
+    done = 0;
+}
+
+uint8_t next(owire_t *device)
+{
+    uint8_t more_searches = FALSE;  // return variable - indicates if more searching needs to be done
+    uint8_t rom_bit_index = 1;      // bit index in ROM array
+    uint8_t rom_byte_index = 0;     // byte index in ROM array
+    uint8_t discrepancy_marker = 0; // signals where a discrepancy was detected
+    uint8_t read_bits = 0;          // true and false bits read from the devices
+    uint8_t rom_bit = 0;            // ROM bit to use
+    uint8_t mask = 1;               // bit mask for current ROM byte
+
+    if (done)
     {
-        owire_write_byte(sensor->device, DS18B20_ROM_SKIP);
-        owire_write_byte(sensor->device, DS18B20_CONVERT_T);
+        done = 0;
+        return FALSE;
+    }
 
-        // wait for conversion to finish
-        while (!owire_read(sensor->device));
+    if (!owire_reset_pulse(device))
+    {
+        last_discrepancy = 0;
+        return FALSE;
+    }
 
-        if (owire_reset_pulse(sensor->device))
+    owire_write_byte(device, DS18B20_ROM_SEARCH);  // send search ROM command
+
+    // collect all 8 ROM bytes
+    while (rom_byte_index < 8)
+    {
+        if (owire_read_bit(device) == 1) // read true value of ROM bit
+            read_bits = 2;
+        if (owire_read_bit(device) == 1) // read false value of ROM bit
+            read_bits |= 1;
+        if (read_bits == 3) // no devices are on the 1-Wire bus
+            break;
+
+        if (read_bits > 0)
+            rom_bit = read_bits >> 1; // capture the true bit (will be 0 or 1)
+        else
         {
-            owire_write_byte(sensor->device, DS18B20_ROM_SKIP);
-            owire_write_byte(sensor->device, DS18B20_READ_SCRATCHPAD);
-            for (lcv = 0; lcv < 9; lcv++)
-            {
-                sensor->scratchpad[lcv] = owire_read_byte(sensor->device);
-            }
+            // if this discrepancy is before the last discrepancy on a previous
+            // next then something went wrong; repeat and pick the same as last
+            // time
+            if (rom_bit_index < last_discrepancy)
+                rom_bit = (latest_ROM[rom_byte_index] & mask) > 0;
+            else
+                rom_bit = rom_bit_index == last_discrepancy;
+
+            // a new discrepancy has been reached, mark its position
+            if (rom_bit == 0)
+                discrepancy_marker = rom_bit_index;
         }
+
+        // update ROM bits with new output bit
+        if (rom_bit == 1)
+            latest_ROM[rom_byte_index] |= mask;
+        else
+            latest_ROM[rom_byte_index] &= ~mask;
+
+        owire_write_bit(device, rom_bit); // send ROM bit to 1-Wire bus
+        rom_bit_index++;  // move to next rom bit position
+        mask = mask << 1; // shift bit mask to new rom bit position
+
+        // if the mask rolls over back to 0, then go to next ROM byte
+        if (mask == 0)
+        {
+            // TODO: should do a CRC check here on ROM[rom_byte_index]
+            rom_byte_index++;
+            mask = 1;
+        }
+    }
+
+    if (rom_bit_index < 65)
+    {
+        // search was unsuccessful if not all 64 bits were collected
+        last_discrepancy = 0;
     }
     else
     {
-        sensor->status |= DS18B20_STATUS_NO_PRESENCE;
+        // search was successful
+        last_discrepancy = discrepancy_marker;
+        done = last_discrepancy == 0;
+        more_searches = TRUE;
+    }
+
+    return more_searches;
+}
+
+uint8_t first(owire_t *device)
+{
+    last_discrepancy = 0;
+    done = 0;
+    return next(device);
+}
+
+void ds18b20_find_devices(temp_sensors_t *sensors)
+{
+    uint8_t num_roms;
+    uint8_t lcv;
+    setup();
+    if (owire_reset_pulse(sensors->bus))
+    {
+        if (first(sensors->bus))
+        {
+            num_roms = 0;
+            do
+            {
+                for (lcv = 0; lcv < 8; lcv++)
+                {
+                    sensors->ROMS[num_roms][lcv] = latest_ROM[lcv];
+                }
+                num_roms++;
+            } while (next(sensors->bus) && num_roms < MAX_TEMP_SENSORS);  // find all devices
+        }
+    }
+}
+
+void match_rom(owire_t *bus, uint8_t *ROM)
+{
+    uint8_t lcv;
+    owire_write_byte(bus, DS18B20_ROM_MATCH);
+    for (lcv = 0; lcv < 8; lcv++)
+    {
+        owire_write_bit(ROM[lcv]);
+    }
+}
+
+void ds18b20_convert_temp(temp_sensors_t *sensors, uint8_t *ROM)
+{
+    // TODO: disable interrupts while communicating to device
+    uint8_t lcv;
+    owire_reset_pulse(sensors->bus)
+    match_rom(sensors->bus, ROM);
+    owire_write_byte(sensors->bus, DS18B20_CONVERT_TEMP);
+
+    // wait for conversion to finish
+    while (!owire_read(sensors->bus));
+
+    owire_reset_pulse(sensors->bus)
+    match_rom(sensors->bus, ROM);
+    owire_write_byte(sensors->bus, DS18B20_READ_SCRATCHPAD);
+    for (lcv = 0; lcv < 9; lcv++)
+    {
+        scratchpad[lcv] = owire_read_byte(sensors->bus);
     }
     // TODO: re-enable interrupts when communication is done
+}
+
+uint8_t ds18b20_temp_hi()
+{
+    return scratchpad[1];
+}
+
+uint8_t ds18b20_temp_lo()
+{
+    return scratchpad[0];
 }
